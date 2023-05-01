15a43b1c5bf5c481ec9d00e3f3625419ebf455d3
Crowdbotics <engineering@crowdbotics.com>
2023-05-01 11:56:50 +0000

    Update store.

--- store/testDanPets/api.js
+++ store/testDanPets/api.js
@@ -3,7 +3,7 @@ const testDanPets = axios.create({
   baseURL: "https://www.test.com/api",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-function testdanpets_get_pets_id_read(payload) {
-  return testDanPets.get(`/pets/${payload.id}`)
+function testdanpets_get_pets_category_id_read(payload) {
+  return testDanPets.get(`/pets/${payload.category}/${payload.id}`)
 }
-export const apiService = { testdanpets_get_pets_id_read }
+export const apiService = { testdanpets_get_pets_category_id_read }
